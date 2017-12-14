a99346f800403359bd793c8bf6cd6e214450b470
alex-ly <alex.ly@uoit.net>
2017-12-13 20:30:41 -0500

    Add files via upload

--- java files/LoginActivity.java
+++ java files/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -46,10 +47,14 @@ public class LoginActivity extends AppCompatActivity {
 
         if(!exists){
             //Log.i("Lab3", @string/fail);
-            intent.putExtra("MainMenuFinalProject", "Username not Recognized");
+            //intent.putExtra("MainMenuFinalProject", "Username not Recognized");
+            Toast.makeText(getApplicationContext(), "Username not recognized", Toast.LENGTH_SHORT).show();
+
+        } else{
+            setResult(1, intent);
+            finish();
+
         }
-        setResult(1, intent);
-        finish();
 
         //Log.i("Lab3","Login success");
     }
